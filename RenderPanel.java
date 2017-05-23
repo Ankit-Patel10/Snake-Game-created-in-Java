e74fdd1c2fcdb828cd38c125cd26b2e56a7e6264
EulerLoop <ankitp5469@gmail.com>
2017-05-23 15:00:39 -0400

    Update RenderPanel.java

--- RenderPanel.java
+++ RenderPanel.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 /**
- * @author Jaryt Bustard
+ * @author EulerLoop (Ankit Patel)
  */
 public class RenderPanel extends JPanel
 {
@@ -19,43 +19,30 @@ public class RenderPanel extends JPanel
 	protected void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		
 		Snake snake = Snake.snake;
-
 		g.setColor(GREEN);
-		
 		g.fillRect(0, 0, 800, 700);
-
 		g.setColor(Color.BLUE);
-
 		for (Point point : snake.snakeParts)
 		{
 			g.fillRect(point.x * Snake.SCALE, point.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
 		}
 		
 		g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
-		
 		g.setColor(Color.RED);
-		
 		g.fillRect(snake.cherry.x * Snake.SCALE, snake.cherry.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
-		
 		String string = "Score: " + snake.score + ", Length: " + snake.tailLength + ", Time: " + snake.time / 20;
-		
 		g.setColor(Color.white);
-		
 		g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), 10);
-
 		string = "Game Over!";
-
-		if (snake.over)
-		{
+		
+		if (snake.over) {
 			g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), (int) snake.dim.getHeight() / 4);
 		}
 
 		string = "Paused!";
 
-		if (snake.paused && !snake.over)
-		{
+		if (snake.paused && !snake.over) {
 			g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), (int) snake.dim.getHeight() / 4);
 		}
 	}
